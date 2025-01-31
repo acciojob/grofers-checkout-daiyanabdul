47bb975d0b45d6e16eada4b06f51b22c84c8afe4
daiyanabdul <daiyanabdul1@gmail.com>
2025-01-31 20:42:37 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,26 @@
+onst getSumBtn = document.createElement("button");
+getSumBtn.append("Get Total Price");
+document.body.appendChild(getSumBtn);
+
+const getSum = () => {
+    const prices = document.querySelectorAll(".price");
+
+    let total = 0;
+    prices.forEach(price => {
+        total += Number(price.textContent);
+    });
+    let totalRow = document.getElementById("total-row");
+    if (!totalRow) {
+        totalRow = document.createElement("tr");
+        totalRow.id = "total-row";
+
+        const totalCell = document.createElement("td");
+        totalCell.setAttribute("colspan", "2");
+        totalCell.id = "ans"; 
+
+        totalRow.appendChild(totalCell);
+        document.querySelector("table").appendChild(totalRow);
+    }
+    document.getElementById("ans").textContent = Total Price: Rs ${total};
+};
+getSumBtn.addEventListener("click", getSum);
